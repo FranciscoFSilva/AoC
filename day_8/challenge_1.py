b22c2242827d6ee4e8e93fe78587f9d749ce0435
Francisco F. SIlva <22647533+FranciscoFSilva@users.noreply.github.com>
2024-12-09 21:01:14 +0000

    solved day 8

--- day_8/challenge_1.py
+++ day_8/challenge_1.py
@@ -5,4 +5,54 @@ with open('day_8/' + sys.argv[1]) as file:
     data = file.read().strip()
 
 
-print(data)
+data = data.split('\n')
+data = [list(x) for x in data]
+
+R = len(data)
+C = len(data[0])
+
+antenna = set()
+for i in range(R):
+    for j in range(C):
+        if data[i][j] != '.':
+            antenna.add((i,j,data[i][j]))
+
+count = 0
+antenna = list(antenna)
+visited = set()
+while(antenna):
+    fi, fj, fant = antenna.pop()
+    flag = False 
+    for i, j, ant in antenna:
+        if ant == fant:
+            flag = True
+            di = fi-i
+            dj = fj-j
+            ni = fi+di
+            nj = fj+dj
+            while ((ni < R and nj < C) and 
+                (ni >= 0 and nj >= 0) and 
+                (ni < R and nj >= 0) and 
+                (ni >= 0 and nj < C)):
+                if (ni, nj) not in visited:
+                    visited.add((ni, nj))
+                    count += 1  
+                ni += di
+                nj += dj
+            
+            ni = fi-di
+            nj = fj-dj
+            while ((ni < R and nj < C) and 
+                (ni >= 0 and nj >= 0) and 
+                (ni < R and nj >= 0) and 
+                (ni >= 0 and nj < C)):
+                if (ni, nj) not in visited:
+                    visited.add((ni, nj))
+                    count += 1  
+                ni -= di
+                nj -= dj
+            if flag and (fi, fj) not in visited:
+                visited.add((fi, fj))
+                count += 1
+
+print(count)
